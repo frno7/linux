7d6398acf563a929194063ca566ec19bd4696bea
Fredrik Noring <noring@nocrew.org>
2017-07-26 17:59:46 +0200

    Experimental update for Playstation 2
    Squashed commit based on ps2-v2.6.35.14 with contributions from
    Jürgen Urban, Rick Gaiser, et al.

--- arch/mips/mm/cache.c
+++ arch/mips/mm/cache.c
@@ -22,6 +22,7 @@
 
 /* Cache operations. */
 void (*flush_cache_all)(void);
+EXPORT_SYMBOL(flush_cache_all);
 void (*__flush_cache_all)(void);
 void (*flush_cache_mm)(struct mm_struct *mm);
 void (*flush_cache_range)(struct vm_area_struct *vma, unsigned long start,
@@ -58,6 +59,8 @@ void (*_dma_cache_wback)(unsigned long start, unsigned long size);
 void (*_dma_cache_inv)(unsigned long start, unsigned long size);
 
 EXPORT_SYMBOL(_dma_cache_wback_inv);
+EXPORT_SYMBOL(_dma_cache_wback);
+EXPORT_SYMBOL(_dma_cache_inv);
 
 #endif /* CONFIG_DMA_NONCOHERENT || CONFIG_DMA_MAYBE_COHERENT */
 
@@ -147,6 +150,15 @@ void __update_cache(struct vm_area_struct *vma, unsigned long address,
 		if (exec || pages_do_alias(addr, address & PAGE_MASK))
 			flush_data_cache_page(addr);
 		ClearPageDcacheDirty(page);
+	} else {
+		/* Cache can be dirty when page is not writeable, because
+		 * data is copied to page in kernel.
+		 */
+		address = (unsigned long) page_address(page);
+		if (!cpu_has_ic_fills_f_dc) {
+			flush_data_cache_page(address);
+			/* TBD: Flush instruction cache also? */
+		}
 	}
 }
 
