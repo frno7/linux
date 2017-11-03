2f0dff294ae06a17c8edde10a003078594461f5d
Fredrik Noring <noring@nocrew.org>
2017-11-03 17:56:06 +0000

    MIPS: PS2: PlayStation 2 devices
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

---
+++ arch/mips/include/asm/mach-ps2/cpu-feature-overrides.h
@@ -0,0 +1,36 @@
+/*
+ * PlayStation 2 CPU features
+ *
+ * Copyright (C) 2010-2013 Jürgen Urban
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef __ASM_MACH_PS2_CPU_FEATURE_OVERRIDES_H
+#define __ASM_MACH_PS2_CPU_FEATURE_OVERRIDES_H
+
+#define cpu_has_llsc			0
+#define cpu_has_4k_cache		1
+#define cpu_has_divec			1
+#define cpu_has_4kex			1
+#define cpu_has_counter			1
+#define cpu_has_cache_cdex_p		0
+#define cpu_has_cache_cdex_s		0
+#define cpu_has_mcheck			0
+#define cpu_has_nofpuex			1
+#define cpu_has_mipsmt			0
+#define cpu_has_vce			0
+#define cpu_has_dsp			0
+#define cpu_has_userlocal		0
+#define cpu_has_64bit_addresses		0
+#define cpu_has_64bit   		1
+#define cpu_has_64bit_gp_regs		0
+#define cpu_has_64bit_zero_reg		0
+#define cpu_vmbits			31
+#define cpu_has_clo_clz			0
+#define cpu_has_ejtag			0
+#define cpu_has_ic_fills_f_dc		0
+#define cpu_has_inclusive_pcaches	0
+#define cpu_has_fpu 			0
+
+#endif /* __ASM_MACH_PS2_CPU_FEATURE_OVERRIDES_H */
---
+++ arch/mips/ps2/devices.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * PlayStation 2 devices
+ *
+ * Copyright (C) 2019 Fredrik Noring
+ */
+
+#include <linux/init.h>
+#include <linux/ioport.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/platform_device.h>
