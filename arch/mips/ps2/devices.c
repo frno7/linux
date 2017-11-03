f88dafe9e287656d9cf28ca2990d31174aed26a4
Fredrik Noring <noring@nocrew.org>
2017-11-03 18:41:06 +0000

    MIPS: PS2: Real-time clock (RTC) driver
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

--- arch/mips/ps2/devices.c
+++ arch/mips/ps2/devices.c
@@ -10,3 +10,19 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/platform_device.h>
+
+static struct platform_device rtc_device = {
+	.name		= "rtc-ps2",
+	.id		= -1,
+};
+
+static struct platform_device *ps2_platform_devices[] __initdata = {
+	&rtc_device,
+};
+
+static int __init ps2_device_setup(void)
+{
+	return platform_add_devices(ps2_platform_devices,
+		ARRAY_SIZE(ps2_platform_devices));
+}
+device_initcall(ps2_device_setup);
