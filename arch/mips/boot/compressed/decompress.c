0e00df28c4c4b48ee5ae1e23cbddceaadc6a739d
Fredrik Noring <noring@nocrew.org>
2018-06-24 10:20:37 +0000

    MIPS: Define and use cpu_relax_forever() for various halting loops
    Using cpu_relax_forever() can lower processor energy consumption when
    halting indefinitely.
    
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

--- arch/mips/boot/compressed/decompress.c
+++ arch/mips/boot/compressed/decompress.c
@@ -17,6 +17,7 @@
 #include <linux/libfdt.h>
 
 #include <asm/addrspace.h>
+#include <asm/processor.h>
 
 /*
  * These two variables specify the free mem region
@@ -45,8 +46,7 @@ void error(char *x)
 	puts(x);
 	puts("\n\n -- System halted");
 
-	while (1)
-		;	/* Halt */
+	cpu_relax_forever();	/* Halt */
 }
 
 /* activate the code for pre-boot environment */
