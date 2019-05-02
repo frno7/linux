dca001a0b2ae5eb2a507f4f521a9e04c6e5d4c73
Fredrik Noring <noring@nocrew.org>
2019-05-02 11:39:09 +0200

    MIPS: PS2: GS: Privileged registers read
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

--- arch/mips/include/asm/mach-ps2/gs-registers.h
+++ arch/mips/include/asm/mach-ps2/gs-registers.h
@@ -317,11 +317,15 @@ struct gs_siglblid {
 #define GS_DECLARE_VALID_REG(reg)			\
 	bool gs_valid_##reg(void)
 
+#define GS_DECLARE_RQ_REG(reg)				\
+	u64 gs_readq_##reg(void)
+
 #define GS_DECLARE_WQ_REG(reg)				\
 	void gs_writeq_##reg(u64 value)
 
 #define GS_DECLARE_RW_REG(reg)				\
 	GS_DECLARE_VALID_REG(reg);			\
+	GS_DECLARE_RQ_REG(reg);				\
 	GS_DECLARE_WQ_REG(reg)
 
 /*
@@ -329,6 +333,7 @@ struct gs_siglblid {
  * the following functions:
  *
  *	bool gs_valid_csr(void);
+ *	u64 gs_readq_csr(void);
  *	void gs_writeq_csr(u64 value);
  *
  * gs_valid_csr indicates whether CSR is readable, which is always true,
