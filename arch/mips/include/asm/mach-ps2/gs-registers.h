3f13cb7304659e0d1ce9fd5fce8812e63ad6d689
Fredrik Noring <noring@nocrew.org>
2019-05-02 11:25:00 +0200

    MIPS: PS2: GS: gs_xorq_imr
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

--- arch/mips/include/asm/mach-ps2/gs-registers.h
+++ arch/mips/include/asm/mach-ps2/gs-registers.h
@@ -372,4 +372,7 @@ GS_DECLARE_RW_REG(imr,      imr);
 GS_DECLARE_RW_REG(busdir,   busdir);
 GS_DECLARE_RW_REG(siglblid, siglblid);
 
+/* Exclusive or (XOR) value with the IMR register, and return the result. */
+u64 gs_xorq_imr(u64 value);
+
 #endif /* __ASM_PS2_GS_REGISTERS_H */
