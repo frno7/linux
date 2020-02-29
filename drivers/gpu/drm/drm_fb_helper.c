ce0cb4a0d1ecb1430a4bd6ee9e02034115c63e66
Fredrik Noring <noring@nocrew.org>
2020-02-29 17:52:15 +0100

    DRM: EXPERIMENTAL: PlayStation 2 Graphics Synthesizer
    Compile with CONFIG_DRM_GS=m and replace "modprobe ps2fb" with
    "modprobe gs". At the moment, 576x460 resolution is hardcoded, but
    eventually the video kernel parameter will be supported to configure
    for example "video=AV-MULTI-OUT:1920x1080@50". Also, although the
    fbset command is poorly supported by the DRM, it seems worthwhile to
    try to get it working too.
    
    Substantial clean-ups and bug fixes remain.

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -1486,7 +1486,7 @@ EXPORT_SYMBOL(drm_fb_helper_pan_display);
  * Allocates the backing storage and sets up the fbdev info structure through
  * the ->fb_probe callback.
  */
-static int drm_fb_helper_single_fb_probe(struct drm_fb_helper *fb_helper,
+int drm_fb_helper_single_fb_probe(struct drm_fb_helper *fb_helper,
 					 int preferred_bpp)
 {
 	struct drm_client_dev *client = &fb_helper->client;
@@ -1654,6 +1654,7 @@ static int drm_fb_helper_single_fb_probe(struct drm_fb_helper *fb_helper,
 	strcpy(fb_helper->fb->comm, "[fbcon]");
 	return 0;
 }
+EXPORT_SYMBOL(drm_fb_helper_single_fb_probe);
 
 static void drm_fb_helper_fill_fix(struct fb_info *info, uint32_t pitch,
 				   uint32_t depth)
@@ -1732,7 +1733,7 @@ EXPORT_SYMBOL(drm_fb_helper_fill_info);
  * So, any setup that touches those fields needs to be done here instead of in
  * drm_setup_crtcs().
  */
-static void drm_setup_crtcs_fb(struct drm_fb_helper *fb_helper)
+void drm_setup_crtcs_fb(struct drm_fb_helper *fb_helper)
 {
 	struct drm_client_dev *client = &fb_helper->client;
 	struct drm_connector_list_iter conn_iter;
@@ -1790,6 +1791,7 @@ static void drm_setup_crtcs_fb(struct drm_fb_helper *fb_helper)
 		info->fbcon_rotate_hint = FB_ROTATE_UR;
 	}
 }
+EXPORT_SYMBOL(drm_setup_crtcs_fb);
 
 /* Note: Drops fb_helper->lock before returning. */
 static int
